b0a08aec02688962c1a80e3bc271c943ba304699
vtcanlas <vincentcanlas2000@gmail.com>
2023-07-26 15:49:47 +0800

    Added functions for MDAS operations

--- src/routes/api/calculate/+server.js
+++ src/routes/api/calculate/+server.js
@@ -7,7 +7,8 @@ export async function POST({ request }) {
 }
 
 function calculateExpression(expression) {
-  function applyOperator(a, operator, b) {
+
+  function applyOperator(a, operator, b) { //function for applying operation
     switch (operator) {
       case '+':
         return a + b;
@@ -20,18 +21,67 @@ function calculateExpression(expression) {
     }
   }
 
-  const operators = expression.split(/[0-9.]+/).filter(Boolean);
-  const numbers = expression.split(/[-+*/]/).map(parseFloat);
-  let result = numbers[0];
-
-  for (let i = 0; i < operators.length; i++) {
-    const operator = operators[i];
-    const nextNumber = numbers[i + 1];
-    const partialResult = applyOperator(result, operator, nextNumber);
-    result = partialResult;
+  function getPrecedence(operator) { //function for gettingPrecedence (MDAS)
+    switch (operator) {
+      case '+':
+      case '-':
+        return 1;
+      case '*':
+      case '/':
+        return 2;
+      default:
+        return 0;
+    }
   }
 
-  return result;
+  function infixToPostfix(tokens) { //convert infix to postfix to follow operator precedence
+    const outputQueue = [];
+    const operatorStack = [];
+
+    for (let token of tokens) {
+      if (!isNaN(token)) {         // if token is number
+        outputQueue.push(token);   // push to queue
+      } else {
+        while (
+          operatorStack.length > 0 &&   //operator stack not empty
+          getPrecedence(operatorStack[operatorStack.length - 1]) >= getPrecedence(token)  //precedence of operator at the top is greater than or equal to precednece of current token
+        ) {
+          outputQueue.push(operatorStack.pop()); //pop operator at top of the stack and push it to outputQueue
+        }
+        operatorStack.push(token); 
+      }
+    }
+
+    while (operatorStack.length > 0) {
+      outputQueue.push(operatorStack.pop());
+    }
+
+    return outputQueue;
+  }
+
+  const tokens = expression.split(/(\+|\-|\*|\/)/).map((token) => {
+    // Convert numbers to floats and leave operators as they are
+    return isNaN(token) ? token : parseFloat(token);
+  });
+
+  const postfix = infixToPostfix(tokens);
+
+  const stack = [];
+  for (let token of postfix) {
+    if (!isNaN(token)) {
+      stack.push(token);
+    } else {
+      const b = stack.pop();
+      const a = stack.pop();
+      const result = applyOperator(a, token, b);
+      stack.push(result);
+    }
+  }
+
+  return stack[0];
 }
+
+
+
 
 
