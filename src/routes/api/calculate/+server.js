ef873074e2064c82d6ff5ad3447ff268ed0827de
vtcanlas <vincentcanlas2000@gmail.com>
2023-07-26 10:16:19 +0800

    removed mathjs from +server.js and replaced with custom function

--- src/routes/api/calculate/+server.js
+++ src/routes/api/calculate/+server.js
@@ -1,13 +1,37 @@
-// api/calculate.js
-
 import { json } from '@sveltejs/kit';
-import math from 'mathjs';
-
 /** @type {import('./$types').RequestHandler} */
 
 export async function POST({ request }) {
   const { expression } = await request.json();
-  return json(math.evaluate(expression));
+  return json(calculateExpression(expression));
+}
+
+function calculateExpression(expression) {
+  function applyOperator(a, operator, b) {
+    switch (operator) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      case '*':
+        return a * b;
+      case '/':
+        return a / b;
+    }
+  }
+
+  const operators = expression.split(/[0-9.]+/).filter(Boolean);
+  const numbers = expression.split(/[-+*/]/).map(parseFloat);
+  let result = numbers[0];
+
+  for (let i = 0; i < operators.length; i++) {
+    const operator = operators[i];
+    const nextNumber = numbers[i + 1];
+    const partialResult = applyOperator(result, operator, nextNumber);
+    result = partialResult;
+  }
+
+  return result;
 }
 
 
